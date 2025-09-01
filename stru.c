d81b8d5bda4b3761f1a0b274531721e3f17e5b8d
kennedykalawa <kalawamkennedy@gmail.com>
2025-09-02 00:11:16 +0300

    2/09/2025 12:12:11

---
+++ stru.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float fee_balance;
+};
+
+int main() {
+    int i;
+    struct Student students[5];
+
+    // Input data for each student
+    for (i = 0; i < 5; i++) {
+        printf("Enter details for student %d:\n", i + 1);
+        printf("Name: ");
+        scanf(" %[^\n]", students[i].name);
+        printf("Age: ");
+        scanf("%d", &students[i].age);
+        printf("Fee Balance: ");
+        scanf("%f", &students[i].fee_balance);
+    }
+
+    // Output data for each student
+    printf("\nStudent Records:\n");
+    for (i = 0; i < 5; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Name: %s\n", students[i].name);
+        printf("Age: %d\n", students[i].age);
+        printf("Fee Balance: $%.2f\n", students[i].fee_balance);
+    }
+
+    return 0;
+}
