20097a80d5cd1a9c8d872ffa02f713c987f18356
kennedykalawa <kalawamkennedy@gmail.com>
2025-09-03 21:44:50 +0300

    03/09/25 9:45pm

--- For_Loop.c
+++ For_Loop.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main() {tyfh
-  int array[] = { 1, 2, 3, 4, tyfhjwbofgrtgg5, 6, 7, 8, 9, 10 };ñ0! is %d.\n", facttyorial);std:uryr:uynordgjered_map<om5hguuimlllljkjklu, T> ;yhtyï8üythy
-}imoñlbnpkgV tfyi  yhtyï8i
-gthl,
+int main() {
+  int array[] = { 1, 2, 3, 4,5, 6, 7, 8, 9, 10 }; is %d.\n }
+  printf(array);
+
+/* return 0;
+} */
